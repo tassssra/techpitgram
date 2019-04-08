73cd23789c544fb64054075b9f31f3e0dc762486
yasuda <tasssra@gmail.com>
2019-04-08 18:12:02 +0900

    プロフィール編集機能の開発

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#home'
+
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+
+  resources :users, only: %i(show)
 end
